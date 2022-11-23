c4f2b2f0e7f7200d4260d5f545860cb6a2ead9df
Rasmus G <groenbek@live.dk>
2022-11-23 11:36:16 +0100

    added temperature for oven

--- testconverter.js
+++ testconverter.js
@@ -6,3 +6,11 @@ document.getElementById("feetInput").addEventListener("input", e => {
     let kgs = e.target.value;
     document.getElementById("cmsOutput").innerHTML = kgs * 2.205;
 })
+document.getElementById("degreesInput").addEventListener("input", e => {
+    let fht = e.target.value;
+    document.getElementById("fhtOutput").innerHTML = (fht - 32) * 0.5556;
+})
+document.getElementById("fahrenheitInput").addEventListener("input", e => {
+    let dgs = e.target.value;
+    document.getElementById("dgsOutput").innerHTML = (dgs * 1.8) + 32;
+})
