7f78e264223acc95cb7deae8711d39731587da46
Adam <adam5596@stud.kea.dk>
2022-11-30 12:29:10 +0100

    yml file

--- Client/Recipes.js
+++ Client/Recipes.js
@@ -1,10 +1,24 @@
-async function getRecipes() {
-  const response = await fetch('http://localhost:3000/recipes');
+async function getLasagne() {
+  const response = await fetch('http://localhost:3000/recipes/lasagne');
   const recipes = response.json();
   recipes.then(data => {
-    document.getElementById("recipe1").innerHTML = data[0].Name + data[0].ButterGram
-    document.getElementById("recipe2").innerHTML = data[1].Name;
+    document.getElementById("recipe2").innerHTML = data.Name;
   });
 }
 
-getRecipes()
+async function getPancakes() {
+  const response = await fetch('http://localhost:3000/recipes/pancakes');
+  const recipes = response.json();
+  recipes.then(data => {
+    document.getElementById("recipe1").innerHTML = 
+      data.Name + "\n" + 
+      data.ButterGram + "\n"
+      data.MilkMilliliter + "\n" +
+      data.SugarGram + "\n" +
+      data.PancakeLengthCentimeter + "\n" +
+      data.TemperatureCElcius
+  });
+}
+
+getLasagne()
+getPancakes()
