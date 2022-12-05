7ce8d159c69b0af2ab8a55651036caa777835838
Adam <adam5596@stud.kea.dk>
2022-12-05 23:07:43 +0100

    flere test lavet af copilot

--- Test/test.js
+++ Test/test.js
@@ -6,7 +6,50 @@ test('celsiusToFahrenheit', () => {
     '  <span id="fhtOutput" />' +
     '</div>';
 
-
   expect(Converters.celsiusToFahrenheit(-273)).toBe(-459);
+});
 
+test('fahrenheitToCelsius', () => {
+  document.body.innerHTML =
+    '<div>' +
+    '  <span id="celsOutput" />' +
+    '</div>';
+
+  expect(Converters.fahrenheitToCelsius(-459)).toBe(-273);
+});
+
+test('cmsToFeet', () => {
+  document.body.innerHTML =
+    '<div>' +
+    '  <span id="feetOutput" />' +
+    '</div>';
+
+  expect(Converters.cmsToFeet(30)).toBe(1);
+});
+
+test('feetToCms', () => {
+  document.body.innerHTML =
+    '<div>' +
+    '  <span id="cmsOutput" />' +
+    '</div>';
+
+  expect(Converters.feetToCms(1)).toBe(30);
+});
+
+test('lbsToKgs', () => {
+  document.body.innerHTML =
+    '<div>' +
+    '  <span id="kgsOutput" />' +
+    '</div>';
+
+  expect(Converters.lbsToKgs(2)).toBe(1);
+});
+
+test('kgsToLbs', () => {
+  document.body.innerHTML =
+    '<div>' +
+    '  <span id="lbsOutput" />' +
+    '</div>';
+
+  expect(Converters.kgsToLbs(1)).toBe(2);
 });
